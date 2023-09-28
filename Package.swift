df08e94b272b005c5d9dd3ddbde511c60dd61b02
l2athod <rathodharsh1974@gmail.com>
2023-09-28 11:03:39 +0530

    Perfect-Net initial version

---
+++ Package.swift
@@ -0,0 +1,54 @@
+// swift-tools-version:5.1
+//
+//  Package.swift
+//  PerfectNet
+//
+//  Created by Kyle Jessup on 2016-05-02.
+//	Copyright (C) 2016 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
+import PackageDescription
+
+#if os(Linux)
+let package = Package(
+    name: "PerfectNet",
+    products: [
+        .library(name: "PerfectNet", targets: ["PerfectNet"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "4.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-LinuxBridge.git", from: "3.0.0"),
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0")
+    ],
+    targets: [
+        .target(name: "PerfectNet", dependencies: ["PerfectCrypto", "LinuxBridge", "PerfectThread"]),
+        .testTarget(name: "PerfectNetTests", dependencies: ["PerfectNet", "PerfectCrypto", "PerfectThread"])
+    ]
+)
+#else
+let package = Package(
+    name: "PerfectNet",
+    products: [
+        .library(name: "PerfectNet", targets: ["PerfectNet"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/l2athod/Perfect-Crypto.git", from: "1.0.0"),
+        .package(url: "https://github.com/l2athod/Perfect-Thread.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(name: "PerfectNet", dependencies: ["PerfectCrypto", "PerfectThread"]),
+        .testTarget(name: "PerfectNetTests", dependencies: ["PerfectNet", "PerfectCrypto", "PerfectThread"])
+    ]
+)
+#endif
